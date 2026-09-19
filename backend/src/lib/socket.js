0a5ad9aa8c5a05c59a056e386e05c5353e0a3474
Shivastu Mishra <shivastumishra@gmail.com>
2026-09-19 13:08:21 +0530

    v5

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,8 +7,9 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
-  },
+    origin: "https://dockerized-fullstack.onrender.com",
+    credentials: true
+  }
 });
 
 export function getReceiverSocketId(userId) {
