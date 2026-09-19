6581286b92accc1fe8ee4265f076aad8c56e6416
Shivastu Mishra <shivastumishra@gmail.com>
2026-09-19 12:57:35 +0530

    v4

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://dockerized-fullstack-1.onrender.com/",
+  baseURL: "https://dockerized-fullstack-1.onrender.com/api",
   withCredentials: true,
 });
